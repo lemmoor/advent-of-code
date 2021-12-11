5dd98344449bceb5a1b4a8f9d5e3974c80848b45
lemmoor <49235295+lemmoor@users.noreply.github.com>
2021-12-11 20:18:38 +0100

    day 4 part 2

--- day_4/index.js
+++ day_4/index.js
@@ -3,61 +3,67 @@ const fs = require('fs');
 const numberPile = [72, 99, 88, 8, 59, 61, 96, 92, 2, 70, 1, 32, 18, 10, 95, 33, 20, 31, 66, 43, 26, 24, 91, 44, 11, 15, 48, 90, 27, 29, 14, 68, 3, 50, 69, 74, 54, 4, 16, 55, 64, 12, 73, 80, 58, 83, 6, 87, 30, 41, 25, 39, 93, 60, 9, 81, 63, 75, 46, 19, 78, 51, 21, 28, 94, 7, 17, 42, 53, 13, 97, 98, 34, 76, 89, 23, 86, 52, 79, 85, 67, 84, 47, 22, 37, 65, 71, 49, 82, 40, 77, 36, 62, 0, 56, 45, 57, 38, 35, 5]
 const marked = "X";
 
-fs.readFile('boards.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
+class Board {
+    constructor(n) {
+        this.numbers = n
+        this.hasWon = false;
     }
 
-    data = data.split(/\r?\n/);
-
-    //im sure there's a better way to do it but idk
-    let boards = [];
-    for (let i = 0; i < data.length - 4; i++) {
-        let board = [];
-        board.push(data[i].trim().split(/\s+/).map(n => parseInt(n, 10)));
-        board.push(data[i + 1].trim().split(/\s+/).map(n => parseInt(n, 10)));
-        board.push(data[i + 2].trim().split(/\s+/).map(n => parseInt(n, 10)));
-        board.push(data[i + 3].trim().split(/\s+/).map(n => parseInt(n, 10)));
-        board.push(data[i + 4].trim().split(/\s+/).map(n => parseInt(n, 10)));
-        boards.push(board);
-    }
-
-    let win = false;
-    let drawnNum = 0;
-    while (drawnNum < 18 && !win) {
-        boards.forEach(board => {
-            checkNum(board, numberPile[drawnNum])
-            if (drawnNum > 4) win = checkWin(board);
-            if (win) {
-                console.log(win)
-                countScore(board, numberPile[drawnNum])
-            }
+    checkNum(n) {
+        this.numbers.forEach(row => {
+            row.forEach((num, i) => { if (num == n) row[i] = marked })
         });
-
-        drawnNum++;
     }
-});
 
-function checkNum(board, n) {
-    board.forEach(row => {
-        row.forEach((num, i) => { if (num == n) row[i] = marked })
-    });
-}
-
-function checkWin(board) {
-    for (let i = 0; i < board.length; i++) {
-        if (board[i].every(x => x == marked)) return true;
-        if (board[0][i] == marked && board[1][i] == marked && board[2][i] == marked && board[3][i] == marked && board[4][i] == marked) return true;
+    checkWin() {
+        for (let i = 0; i < this.numbers.length; i++) {
+            if (this.numbers[i].every(x => x == marked)) this.hasWon = true;
+            if (this.numbers.map(row => row[i]).every(x => x === marked)) this.hasWon = true;
+        }
     }
-    return false;
+
+    countScore(n) {
+        let sum = 0;
+        this.numbers.forEach(row => {
+            row.forEach(num => { if (num != marked) sum += num; })
+        })
+
+        return sum * n;
+    }
 }
 
-function countScore(board, n) {
-    let sum = 0;
-    board.forEach(row => {
-        row.forEach(num => { if (!isNaN(num)) sum += num; })
-    })
+const lines = fs
+    .readFileSync("boards.txt", { encoding: "utf-8" })
+    .split(/\r?\n/)
+    .map((x) =>
+        x
+            .replace(/[\n ,]+/g, " ")
+            .trim()
+            .split(" ")
+            .map(Number)
+    );
 
-    console.log(sum * n);
-    return sum * n;
+const boards = [];
+
+for (let i = 0; i < lines.length - 1; i += 5) {
+    boards.push(new Board([lines[i], lines[i + 1], lines[i + 2], lines[i + 3], lines[i + 4]]))
 }
+
+let boardScores = []
+let drawnNum = 0;
+while (drawnNum < numberPile.length) {
+    for (const board of boards) {
+        if (!board.hasWon) {
+            board.checkNum(numberPile[drawnNum])
+            if (drawnNum > 4) board.checkWin();
+            if (board.hasWon) {
+                boardScores.push(board.countScore(numberPile[drawnNum]))
+            }
+        }
+
+    }
+    drawnNum++;
+}
+
+console.log("PART 1: ", boardScores[0])
+console.log("PART 2: ", boardScores[boardScores.length - 1])
