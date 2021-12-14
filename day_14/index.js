bec05c211458cf28b5e686f795d343a918ca4432
lemmoor <49235295+lemmoor@users.noreply.github.com>
2021-12-14 20:01:51 +0100

    day 14 part 2

--- day_14/index.js
+++ day_14/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { METHODS } = require('http')
 
 let pairRules = new Map()
 let [template, rules] = fs.readFileSync('input.txt', { encoding: "utf-8" })
@@ -10,6 +11,45 @@ rules.split(/\r?\n/).map(x => {
 })
 
 part1(template, pairRules)
+part2(template, pairRules)
+
+function part2(template, pairRules) {
+    let pairCount = {}
+    for (let i = 0; i < template.length - 1; i++) {
+        pairCount[template[i] + template[i + 1]] = pairCount[template[i] + template[i + 1]] === undefined ? 1 : pairCount[template[i] + template[i + 1]] + 1;
+    }
+
+    for (let i = 0; i < 40; i++) {
+        pairCount = part2_step(pairRules, pairCount)
+    }
+
+    console.log(countElements2(pairCount))
+}
+
+function part2_step(pairRules, pairCount) {
+    let newPairs = {}
+    let pairs = Object.keys(pairCount)
+    for (const key of pairs) {
+        let [first, sec] = [...key]
+        let insert = pairRules.get(key)
+        newPairs[first + insert] = newPairs[first + insert] === undefined ? pairCount[key] : newPairs[first + insert] + pairCount[key];
+        newPairs[insert + sec] = newPairs[insert + sec] === undefined ? pairCount[key] : newPairs[insert + sec] + pairCount[key];
+    }
+    return newPairs;
+}
+
+function countElements2(pairs) {
+    let count = {}
+    let keys = Object.keys(pairs)
+    for (const key of keys) {
+        let [a, b] = [...key]
+        count[a] = count[a] === undefined ? pairs[key] : count[a] + pairs[key];
+        count[b] = count[b] === undefined ? pairs[key] : count[b] + pairs[key];
+    }
+    let t = Object.values(count).map(x => Math.ceil(x / 2))
+    return (Math.max(...t) - Math.min(...t));
+}
+
 function part1(template, pairRules) {
     for (let i = 0; i < 10; i++) {
         template = part1_step(template, pairRules)
