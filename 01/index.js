7a7db291283f2385fe2d68d379af5e7876b2a0d6
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-12-01 07:08:50 +0100

    day 1

--- 01/index.js
+++ 01/index.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
-const data = fs.readFileSync('input.txt', { encoding: "utf-8" }).split(/\r?\n/);
+const path = require('node:path');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n\r?\n/).map(e => e.split(/\r?\n/).map(Number));
+
+//console.log(input)
+
+let most = []
+
+for(elf of input){
+    let sum = 0;
+    for(cal of elf){
+        sum+=cal
+    }
+    most.push(sum) 
+}
+
+most = most.sort((a, b) => b - a)
+console.log("part 1 ", most[0], "part 2: ", most[0] + most[1] + most[2]);
