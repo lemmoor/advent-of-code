b0c903a92e8d1586b8d9604dc7cd534c12eca204
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-12-03 12:43:59 +0100

    template change

--- 00/index.js
+++ 00/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const path = require('node:path');
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n\r?\n/)
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n/)
 
 console.log(input)
