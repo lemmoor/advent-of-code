84b00316642fb823c056b0eba34bc18ec3ddc1ab
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-12-02 17:20:28 +0100

    cleanup

--- 01/index.js
+++ 01/index.js
@@ -1,18 +1,45 @@
 const fs = require('fs');
 const path = require('node:path');
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n\r?\n/).map(e => e.split(/\r?\n/).map(Number));
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n/)
 
-//console.log(input)
+// console.log(input)
 
-let most = []
+let sum = 0;
+input.forEach(line => {
+    let numbers = [...line.matchAll(/\d/g)];
+    sum += Number(numbers[0][0] + numbers[numbers.length - 1][0]);
+})
 
-for(elf of input){
-    let sum = 0;
-    for(cal of elf){
-        sum+=cal
+console.log('part 1: ', sum)
+
+let sum2 = 0;
+let regex = /.*?(\d|two|one|three|four|five|six|seven|eight|nine)(?:.*(\d|two|one|three|four|five|six|seven|eight|nine))?/g;
+
+input.forEach(line => {
+    let numbers = [...line.matchAll(regex)];
+    let n1 = numbers[0][1];
+    let n2 = numbers[0][2];
+    if (!n2) {
+        n2 = n1;
     }
-    most.push(sum) 
-}
+    sum2 += convertToNum(n1) * 10 + convertToNum(n2);
+})
 
-most = most.sort((a, b) => b - a)
-console.log("part 1 ", most[0], "part 2: ", most[0] + most[1] + most[2]);
+console.log('part 2:', sum2);
+
+function convertToNum(str) {
+    const converter = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+
+    if (isNaN(str)) return converter[str];
+    return Number(str);
+}
