5e6987714f2d14e9b5fe764ab6af9a8ef434c0e0
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-12-04 16:54:03 +0100

    part 2

--- 04/index.js
+++ 04/index.js
@@ -1,27 +1,38 @@
 const fs = require('fs');
 const path = require('node:path');
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: "utf-8" }).split(/\r?\n/).map(line => {
-    let numbers = line.replace(/Card \d+: /g, '').split(' | ');
+const input = fs
+  .readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf-8' })
+  .split(/\r?\n/)
+  .map((line) => {
+    let numbers = line.replace(/Card *\d+: */g, '').split(' | ');
 
-    return { winning: numbers[0], your: numbers[1] }
-})
+    return { winning: numbers[0], your: numbers[1] };
+  });
 
 // console.log(input)
 
 let sum = 0;
-for (const card of input) {
-    // console.log(card);
-    let winRegex = new RegExp('(?<!\\d)(' + card.winning.trim().replaceAll(/\s+/g, '|') + ')(?!\\d)', 'g');
-    let score = 0;
-    let match;
-    while ((match = winRegex.exec(card.your)) !== null) {
-        // console.log(`Found ${match[0]}. at ${match.index}.`);
-        score = score ? score * 2 : 1;
-        // console.log('score ', score);
-    }
+let cardsAvailable = new Array(input.length).fill(1);
+for (let i = 0; i < input.length; i++) {
+  const card = input[i];
+  let winRegex = new RegExp('(?<!\\d)(' + card.winning.trim().replaceAll(/\s+/g, '|') + ')(?!\\d)', 'g');
+  let score = 0;
+  let numbersMatched = 0;
+  let match;
+  while ((match = winRegex.exec(card.your)) !== null) {
+    score = score ? score * 2 : 1;
+    numbersMatched++;
+  }
 
-    sum += score;
-    score = 0;
+  for (let j = 1; j <= numbersMatched; j++) {
+    cardsAvailable[i + j] += 1 * cardsAvailable[i];
+  }
+
+  sum += score;
 }
 
-console.log(sum);
+console.log('part 1: ', sum);
+console.log(
+  'part 2: ',
+  cardsAvailable.reduce((s, v) => v + s)
+);
