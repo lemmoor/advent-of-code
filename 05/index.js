1fcb1a9ea9e61f1b41b47e3c6f13b5b960a71bfb
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-12-05 18:24:03 +0100

    horribly unoptimised part 2

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('node:path');
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf-8' }).split(/\r?\n\r?\n/);
@@ -8,7 +9,6 @@ input.shift();
 
 const maps = input.map((conversionMap) => {
   let lines = conversionMap.split(/\r?\n/);
-  console.log(lines);
   let map = [];
   for (let i = 1; i < lines.length; i++) {
     const line = lines[i];
@@ -21,10 +21,8 @@ const maps = input.map((conversionMap) => {
 // console.log(seeds, maps);
 
 //part 1
-let count = 0;
 let seedsToLocation = [];
 for (const seed of seeds) {
-  count = 0;
   let sourceNumber = seedToLocation(seed);
   seedsToLocation.push(sourceNumber);
 }
@@ -46,48 +44,17 @@ function seedToLocation(seed) {
 console.log('part 1: ', Math.min(...seedsToLocation));
 
 //part 2
-
-// let seedRangesToLocation = [];
-// for (let i = 0; i < seeds.length; i += 2) {
-//   for (let j = seeds[i]; j <= seeds[i] + seeds[i + 1]; j++) {
-//     let sourceNumber = seedToLocation(j);
-//     seedRangesToLocation.push(sourceNumber);
-//   }
-//   console.log(seedToLocation(seeds[i]), seedToLocation(seeds[i] + seeds[i + 1]));
-// }
-
-// console.log(Math.min(...seedRangesToLocation));
-seedRangesToLocation(seeds);
-function seedRangesToLocation(seedRanges) {
-  let maxRanges = [];
-  //location on each end the ranges
-  for (let i = 0; i < seedRanges.length; i += 2) {
-    maxRanges.push(seedToLocation(seedRanges[i]));
-    maxRanges.push(seedToLocation(seedRanges[i] + seedRanges[i + 1]));
-  }
-
-  //find the smallest range
-  let smallestStart = 0;
-  let smallest = [maxRanges[0], maxRanges[1]];
-  for (let i = 2; i < maxRanges.length; i += 2) {
-    if (smallest[0] + smallest[i] < maxRanges[i] + maxRanges[i + 1]) {
-      smallestStart = i;
-      smallest = [maxRanges[i], maxRanges[i + 1]];
-    }
-  }
-
-  console.log(maxRanges);
-  console.log(smallest, smallestStart);
-
-  let min = Infinity;
-  for (let i = seedRanges[smallestStart]; i <= seedRanges[smallestStart] + seedRanges[smallestStart + 1]; i++) {
+let min = Infinity;
+for (let j = 6; j < seeds.length; j += 2) {
+  console.log('range id:', j);
+  for (let i = seeds[j]; i <= seeds[j] + seeds[j + 1]; i++) {
     let location = seedToLocation(i);
     if (min > location) {
       min = location;
     }
   }
-
-  console.log(min);
+  //console logs to see that the progress because it's that slow
+  console.log('min: ', min);
 }
 
-//316960384 too high
+console.log('part 2:', min);
